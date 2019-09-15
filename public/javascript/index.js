34d55b79fc30df9c51ce650c6d9aeb4f00a7774f
Joe Aiken <jaiken@nvisionative.com>
2019-09-15 14:17:10 -0400

    large amount of updates for nav bar, carousel, sections 1 and 2

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
 	$('.carousel').carousel();
 	$('#hoursModal').trigger("focus");
 	$('#submitted').trigger("focus");
+
+	  /* Add scroll class toggle */
+	  $(function () {
+		$(document).scroll(function () {
+		  var nav = $(".sticky-top");
+		  nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
+		});
+	  });
+	
+	  $('a[href*="#"]').on('click', function (e) {
+		e.preventDefault();
+	
+		$('html, body').animate({
+		  scrollTop: $($(this).attr('href')).offset().top
+		}, 700, 'linear');
+	  });
 });
